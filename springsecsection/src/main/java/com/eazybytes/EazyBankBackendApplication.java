84d5e8cb3377c1329712e318e9506e335b3098a2
sdeshmukh20 <satyajeet.deshmukh08@gmail.com>
2024-07-18 11:38:16 +0530

    section 4

--- springsecsection/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ springsecsection/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*@EnableJpaRepositories("com.eazybytes.repository")
+@EntityScan("com.eazybytes.model")*/
 public class EazyBankBackendApplication {
 
     public static void main(String[] args) {
--- springsecsection/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecsection/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -4,13 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
@@ -21,22 +16,19 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests
+        http.csrf(csrfConfig -> csrfConfig.disable())
+                .authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-                .requestMatchers("/notices", "/contact", "/error").permitAll());
+                .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user").password("{noop}EazyBytes@12345").authorities("read").build();
-        UserDetails admin = User.withUsername("admin")
-                            .password("{bcrypt}$2a$12$88.f6upbBvy0okEa7OfHFuorV29qeK.sVbB9VQ6J6dWM1bW6Qef8m")
-                            .authorities("admin").build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }
+    /*@Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }*/
 
     @Bean
     public PasswordEncoder passwordEncoder() {
