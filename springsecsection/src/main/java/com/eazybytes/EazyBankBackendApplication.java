1457be516df6023e3d2f13d73fe7b0d3b86801fa
sdeshmukh20 <satyajeet.deshmukh08@gmail.com>
2024-07-18 22:37:57 +0530

    section 6

--- springsecsection/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ springsecsection/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-/*@EnableJpaRepositories("com.eazybytes.repository")
-@EntityScan("com.eazybytes.model")*/
 public class EazyBankBackendApplication {
 
     public static void main(String[] args) {
