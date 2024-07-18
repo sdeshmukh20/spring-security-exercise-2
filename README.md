e5e6386ccd9dd20a8e8122ac6d1c931d35f5055a
sdeshmukh20 <satyajeet.deshmukh08@gmail.com>
2024-07-18 10:28:34 +0530

    section 2

--- README.md
+++
@@ -1,1 +0,0 @@
-# spring-security-exercise-2, ok
--- springsecsection/src/test/java/com/eazybytes/EazyBankBackendApplicationTests.java
+++ springsecsection/src/test/java/com/eazybytes/EazyBankBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.eazybytes.springsecsection1;
+package com.eazybytes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
